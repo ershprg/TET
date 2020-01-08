ef446896fea8fa96c88784ebc2e32ed74b013683
Aleksandr.Ershov <aleksandr.ershov@amadeus.com>
2020-01-08 13:36:39 +0100

    Update.sh

--- update.sh
+++ update.sh
@@ -1,33 +1,42 @@
 #!/bin/bash
-wget https://www.transeurotrail.org/wp-content/uploads/gpxsync/AL.gpx
-wget https://www.transeurotrail.org/wp-content/uploads/gpxsync/AND.gpx
-wget https://www.transeurotrail.org/wp-content/uploads/gpxsync/A.gpx
-wget https://www.transeurotrail.org/wp-content/uploads/gpxsync/BY.gpx
-wget https://www.transeurotrail.org/wp-content/uploads/gpxsync/B.gpx
-wget https://www.transeurotrail.org/wp-content/uploads/gpxsync/BIH.gpx
-wget https://www.transeurotrail.org/wp-content/uploads/gpxsync/BG.gpx
-wget https://www.transeurotrail.org/wp-content/uploads/gpxsync/HR.gpx
-wget https://www.transeurotrail.org/wp-content/uploads/gpxsync/DK.gpx
-wget https://www.transeurotrail.org/wp-content/uploads/gpxsync/EST.gpx
-wget https://www.transeurotrail.org/wp-content/uploads/gpxsync/FIN.gpx
-wget https://www.transeurotrail.org/wp-content/uploads/gpxsync/D.gpx
-wget https://www.transeurotrail.org/wp-content/uploads/gpxsync/GR.gpx
-wget https://www.transeurotrail.org/wp-content/uploads/gpxsync/H.gpx
-wget https://www.transeurotrail.org/wp-content/uploads/gpxsync/I.gpx
-wget https://www.transeurotrail.org/wp-content/uploads/gpxsync/LT.gpx
-wget https://www.transeurotrail.org/wp-content/uploads/gpxsync/LV.gpx
-wget https://www.transeurotrail.org/wp-content/uploads/gpxsync/MNE.gpx
-wget https://www.transeurotrail.org/wp-content/uploads/gpxsync/NL.gpx
-wget https://www.transeurotrail.org/wp-content/uploads/gpxsync/N.gpx
-wget https://www.transeurotrail.org/wp-content/uploads/gpxsync/PL.gpx
-wget https://www.transeurotrail.org/wp-content/uploads/gpxsync/P.gpx
-wget https://www.transeurotrail.org/wp-content/uploads/gpxsync/RO.gpx
-wget https://www.transeurotrail.org/wp-content/uploads/gpxsync/SRB.gpx
-wget https://www.transeurotrail.org/wp-content/uploads/gpxsync/SK.gpx
-wget https://www.transeurotrail.org/wp-content/uploads/gpxsync/SLO.gpx
-wget https://www.transeurotrail.org/wp-content/uploads/gpxsync/E.gpx
-wget https://www.transeurotrail.org/wp-content/uploads/gpxsync/S.gpx
-wget https://www.transeurotrail.org/wp-content/uploads/gpxsync/CH.gpx
-wget https://www.transeurotrail.org/wp-content/uploads/gpxsync/TR.gpx
-wget https://www.transeurotrail.org/wp-content/uploads/gpxsync/GB.gpx
-wget https://www.transeurotrail.org/wp-content/uploads/gpxsync/UA.gpx
+#wget -N -q https://www.transeurotrail.org/wp-content/uploads/gpxsync/AL.gpx
+#wget -N -q https://www.transeurotrail.org/wp-content/uploads/gpxsync/AND.gpx
+#wget -N -q https://www.transeurotrail.org/wp-content/uploads/gpxsync/A.gpx
+#wget -N -q https://www.transeurotrail.org/wp-content/uploads/gpxsync/BY.gpx
+#wget -N -q https://www.transeurotrail.org/wp-content/uploads/gpxsync/B.gpx
+#wget -N -q https://www.transeurotrail.org/wp-content/uploads/gpxsync/BIH.gpx
+#wget -N -q https://www.transeurotrail.org/wp-content/uploads/gpxsync/BG.gpx
+#wget -N -q https://www.transeurotrail.org/wp-content/uploads/gpxsync/HR.gpx
+#wget -N -q https://www.transeurotrail.org/wp-content/uploads/gpxsync/DK.gpx
+#wget -N -q https://www.transeurotrail.org/wp-content/uploads/gpxsync/EST.gpx
+#wget -N -q https://www.transeurotrail.org/wp-content/uploads/gpxsync/FIN.gpx
+#wget -N -q https://www.transeurotrail.org/wp-content/uploads/gpxsync/D.gpx
+#wget -N -q https://www.transeurotrail.org/wp-content/uploads/gpxsync/GR.gpx
+#wget -N -q https://www.transeurotrail.org/wp-content/uploads/gpxsync/H.gpx
+#wget -N -q https://www.transeurotrail.org/wp-content/uploads/gpxsync/I.gpx
+#wget -N -q https://www.transeurotrail.org/wp-content/uploads/gpxsync/LT.gpx
+#wget -N -q https://www.transeurotrail.org/wp-content/uploads/gpxsync/LV.gpx
+#wget -N -q https://www.transeurotrail.org/wp-content/uploads/gpxsync/MNE.gpx
+#wget -N -q https://www.transeurotrail.org/wp-content/uploads/gpxsync/NL.gpx
+#wget -N -q https://www.transeurotrail.org/wp-content/uploads/gpxsync/N.gpx
+#wget -N -q https://www.transeurotrail.org/wp-content/uploads/gpxsync/PL.gpx
+#wget -N -q https://www.transeurotrail.org/wp-content/uploads/gpxsync/P.gpx
+#wget -N -q https://www.transeurotrail.org/wp-content/uploads/gpxsync/RO.gpx
+#wget -N -q https://www.transeurotrail.org/wp-content/uploads/gpxsync/SRB.gpx
+#wget -N -q https://www.transeurotrail.org/wp-content/uploads/gpxsync/SK.gpx
+#wget -N -q https://www.transeurotrail.org/wp-content/uploads/gpxsync/SLO.gpx
+#wget -N -q https://www.transeurotrail.org/wp-content/uploads/gpxsync/E.gpx
+#wget -N -q https://www.transeurotrail.org/wp-content/uploads/gpxsync/S.gpx
+#wget -N -q https://www.transeurotrail.org/wp-content/uploads/gpxsync/CH.gpx
+#wget -N -q https://www.transeurotrail.org/wp-content/uploads/gpxsync/TR.gpx
+#wget -N -q https://www.transeurotrail.org/wp-content/uploads/gpxsync/GB.gpx
+#wget -N -q https://www.transeurotrail.org/wp-content/uploads/gpxsync/UA.gpx
+
+git diff --exit-code
+if [ $? -ne 0 ];
+then
+    echo "Committing the differences"
+    git add --ignore-removal *.gpx
+    export V_NOW=`date +"%Y%m%d"`
+    git commit -m "$V_NOW TET"
+fi
